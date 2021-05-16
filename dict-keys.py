e7ffc634bebab9ca0a9589be1a57db863e3ed117
AOrcilla <80306312+AOrcilla@users.noreply.github.com>
2021-05-15 21:33:18 -0700

    Added dict-keys.py via Files

---
+++ dict-keys.py
@@ -0,0 +1,12 @@
+# Variable Nexus
+item1 = input('Enter the first item: ')
+item2 = input('Enter the second: ')
+num1 = input('how many of item1 do you need: ')
+num2 = input('how many of item2 do you need: ')
+
+
+var = dict()
+var[item1] = num1
+var[item2] = num2
+
+print(var)
